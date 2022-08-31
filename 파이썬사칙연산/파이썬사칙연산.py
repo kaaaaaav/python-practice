bea494a019e18af2bde644c76b291925a36b4443
espor <espor@CHESSEL>
2022-08-31 23:17:48 +0900

    초기 커밋.

---
+++ 파이썬사칙연산/파이썬사칙연산.py
@@ -0,0 +1,17 @@
+a=int(input('제곱할 수를 쓰시오 ->  '))
+print(a*a)
+
+b=int(input('아무거나 쓰시오 -> '))
+print(b/(b+1)+b*b)
+
+c=1
+while c<=100:
+    print(c)
+    c=c+1
+
+d=int(input('암거나 ㄱ ->'))
+
+e=0
+while e<d:
+    print('',d)
+    e+=1
